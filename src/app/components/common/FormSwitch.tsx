eb3e554aff01fd6abd7abe35be036b8064bd82c7
Lancea <61652470+blazertooth-99@users.noreply.github.com>
2025-01-21 23:16:24 +0700

    Update Trial form

--- src/app/components/common/FormSwitch.tsx
+++ src/app/components/common/FormSwitch.tsx
@@ -1,42 +1,48 @@
-'use client'
+"use client";
 
-import Image from "next/image"
-import { useState } from 'react'
-import { Facebook, Linkedin, Twitter } from 'lucide-react'
-import { Checkbox } from "@/components/ui/checkbox"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { motion, AnimatePresence } from 'framer-motion'
-import ImgFormFill from "../../public/images/form/formfill.png"
-import ImgFormSubmit from "../../public/images/form/formsubmitted.png"
+import Image from "next/image";
+import { useState } from "react";
+import { Facebook, Linkedin, Twitter } from "lucide-react";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { motion, AnimatePresence } from "framer-motion";
+import ImgFormFill from "../../public/images/form/formfill.png";
+import ImgFormSubmit from "../../public/images/form/formsubmitted.png";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 const FormSwitcher = () => {
-
-  const [isSignUp, setIsSignUp] = useState(false)
-  const [isSubmitted, setIsSubmitted] = useState(false)
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const toggleForm = () => {
-    setIsSignUp(!isSignUp)
+    setIsSignUp(!isSignUp);
     setIsSubmitted((prev) => !prev);
-  }
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+    e.preventDefault();
     //Nanti di delete bjir, buat cek form submit
-    console.log('Form submitted')
-    setIsSubmitted(true)
-  }
+    console.log("Form submitted");
+    setIsSubmitted(true);
+  };
 
   return (
     <div className="flex items-center justify-center max-h-fit p-2">
-      <div className="relative w-full max-w-[900px] h-[600px] bg-gradient-to-br from-pink-400 to-pink-600 rounded-xl shadow-lg overflow-hidden">
-
+      <div className="relative w-full max-w-[900px] h-auto max-h-[600px] bg-gradient-to-br from-pink-400 to-pink-600 rounded-xl shadow-lg overflow-y-auto overflow-x-hidden">
         {/* Sign Up Form */}
-        <div className={`w-1/2 h-full flex flex-col justify-center items-center bg-background transition-opacity duration-500 ease-in-out {opacity-0 z-10' : opacity-100 z-10'}`}>
-          <form onSubmit={handleSubmit} className="w-full max-w-[350px] p-8 space-y-6">
-            <h2 className="text-2xl font-bold text-primary text-center">Create Account</h2>
-            <div className="flex justify-center space-x-4">
+        <div
+          className={`w-1/2 h-full flex flex-col justify-center items-center bg-background transition-opacity duration-500 ease-in-out {opacity-0 z-10' : opacity-100 z-10'}`}
+        >
+          <form
+            onSubmit={handleSubmit}
+            className="w-full max-w-[350px] p-8 space-y-6"
+          >
+            <h2 className="text-2xl font-bold text-primary text-center">
+              Create Account
+            </h2>
+            {/* <div className="flex justify-center space-x-4">
               <Button variant="ghost" size="icon" aria-label="Sign up with Facebook">
                 <Facebook className="w-5 h-5" />
               </Button>
@@ -46,21 +52,69 @@ const FormSwitcher = () => {
               <Button variant="ghost" size="icon" aria-label="Sign up with Twitter">
                 <Twitter className="w-5 h-5" />
               </Button>
-            </div>
-            <p className="text-sm text-center text-muted-foreground">or use your email for registration</p>
+            </div> */}
+            <p className="text-sm text-center text-muted-foreground">
+              or use your email for registration
+            </p>
             <div className="space-y-4">
+              <h1>Enter your parent information</h1>
               <div className="space-y-2">
-                <Label htmlFor="signup-name">Name</Label>
-                <Input id="signup-name" placeholder="Name" />
+                <Label htmlFor="signup-name">Parent name</Label>
+                <Input id="signup-name" placeholder="enter your parent name" />
               </div>
               <div className="space-y-2">
-                <Label htmlFor="signup-email">Email</Label>
-                <Input id="signup-email" type="email" placeholder="Email" />
+                <Label htmlFor="parent-phone">Parent phone number</Label>
+                <Input
+                  id="parent-phone"
+                  type="number"
+                  placeholder="enter your parent phone number"
+                />
               </div>
               <div className="space-y-2">
+                <Label htmlFor="signup-email">Parent email</Label>
+                <Input
+                  id="signup-email"
+                  type="email"
+                  placeholder="enter your parent email"
+                />
+              </div>
+              <div className="pt-10">
+                <h1>Enter your student information</h1>
+                <div className="space-y-2">
+                  <Label htmlFor="student-name">Student name</Label>
+                  <Input id="student-name" placeholder="enter student name" />
+                </div>
+                <div className="space-y-2">
+                  <Label htmlFor="student-age">Age</Label>
+                  <Input
+                    id="student-age"
+                    type="number"
+                    defaultValue="0"
+                    min="0"
+                  />
+                </div>
+                <div className="space-y-2">
+                  <Label htmlFor="signup-email">Experience</Label>
+                  <RadioGroup defaultValue="option-one">
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="option-one" id="option-one" />
+                      <Label htmlFor="option-one">GG EZ</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="option-two" id="option-two" />
+                      <Label htmlFor="option-two">Good</Label>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                      <RadioGroupItem value="option-three" id="option-three" />
+                      <Label htmlFor="option-three">Basic</Label>
+                    </div>
+                  </RadioGroup>
+                </div>
+              </div>
+              {/* <div className="space-y-2">
                 <Label htmlFor="signup-password">Password</Label>
                 <Input id="signup-password" type="password" placeholder="Password" />
-              </div>
+              </div> */}
             </div>
             <div className="flex items-center space-x-2">
               <Checkbox id="terms2" />
@@ -74,12 +128,11 @@ const FormSwitcher = () => {
           </form>
         </div>
 
-
         {/* Switch Panel */}
         <motion.div
           className="absolute top-0 w-1/2 h-full flex flex-col justify-center items-center p-8 bg-gradient-to-br from-sky-300 to-sky-500"
-          animate={{ x: isSubmitted ? '0%' : '100%' }}
-          transition={{ duration: 1, ease: 'easeInOut' }}
+          animate={{ x: isSubmitted ? "0%" : "100%" }}
+          transition={{ duration: 1, ease: "easeInOut" }}
         >
           <div className="text-center space-y-6">
             <div className="relative w-full h-48">
@@ -87,34 +140,54 @@ const FormSwitcher = () => {
                 src={ImgFormFill}
                 alt="Fill form"
                 fill
-                style={{ objectFit: 'contain' }}
-                className={`absolute transition-opacity duration-1000 ease-in-out ${isSubmitted ? 'opacity-0' : 'opacity-100'}`}
+                style={{ objectFit: "contain" }}
+                className={`absolute transition-opacity duration-1000 ease-in-out ${
+                  isSubmitted ? "opacity-0" : "opacity-100"
+                }`}
               />
               {/* Image shown when isSubmitted is true */}
               <Image
                 src={ImgFormSubmit}
                 alt="Form submitted"
                 fill
-                style={{ objectFit: 'contain' }}
-                className={`absolute transition-opacity duration-1000 ease-in-out ${isSubmitted ? 'opacity-100' : 'opacity-0'}`}
+                style={{ objectFit: "contain" }}
+                className={`absolute transition-opacity duration-1000 ease-in-out ${
+                  isSubmitted ? "opacity-100" : "opacity-0"
+                }`}
               />
             </div>
             {isSubmitted ? (
               <>
-                <h2 className="text-2xl font-bold text-primary">Welcome Back!</h2>
-                <p className="text-sm text-primary-foreground">To keep connected with us please login with your personal info</p>
+                <h2 className="text-2xl font-bold text-primary">
+                  Welcome Back!
+                </h2>
+                <p className="text-sm text-primary-foreground">
+                  To keep connected with us please login with your personal info
+                </p>
               </>
             ) : (
               <>
-                <h2 className="text-2xl font-bold text-primary">Hello Friend!</h2>
-                <p className="text-sm text-primary-foreground">Enter your personal details and start journey with us</p>
+                <h2 className="text-2xl font-bold text-primary">
+                  Hello Friend!
+                </h2>
+                <p className="text-sm text-primary-foreground">
+                  Enter your personal details and start journey with us
+                </p>
               </>
             )}
           </div>
         </motion.div>
-        <div className={`absolute inset-0 flex transition-all duration-1000 ease-in-out ${isSubmitted ? 'translate-x-1/2' : 'translate-x-full'}`}>
+        <div
+          className={`absolute inset-0 flex transition-all duration-1000 ease-in-out ${
+            isSubmitted ? "translate-x-1/2" : "translate-x-full"
+          }`}
+        >
           {/* Sign In Form */}
-          <div className={`w-1/2 h-full flex items-center justify-center transition-opacity ease-in-out ${isSubmitted ? 'opacity-100 ' : 'opacity-100'}`}>
+          <div
+            className={`w-1/2 h-full flex items-center justify-center transition-opacity ease-in-out ${
+              isSubmitted ? "opacity-100 " : "opacity-100"
+            }`}
+          >
             {/* After Submit View */}
             <AnimatePresence>
               {isSubmitted && (
@@ -146,7 +219,11 @@ const FormSwitcher = () => {
                     animate={{ y: 0, opacity: 1 }}
                     transition={{ delay: 1.6, duration: 1 }}
                   >
-                    <Button onClick={() => setIsSubmitted(false)} variant="outline" className="w-[180px]">
+                    <Button
+                      onClick={() => setIsSubmitted(false)}
+                      variant="outline"
+                      className="w-[180px]"
+                    >
                       Go Back
                     </Button>
                   </motion.div>
@@ -155,9 +232,8 @@ const FormSwitcher = () => {
             </AnimatePresence>
           </div>
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
 export default FormSwitcher;
