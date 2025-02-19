bd2a3c8ae2d88305c157636921c2866bac886788
christiansatrio35 <christiansatrio35@gmail.com>
2025-02-19 04:12:52 +0000

    Edit Contact us page & add route navbar

--- src/app/(mainpage)/contact/ContactUs.tsx
+++ src/app/(mainpage)/contact/ContactUs.tsx
@@ -12,13 +12,13 @@ const ContactUs = () => {
   return (
     <main>
       {/* Form section */}
-      <div className="grid grid-cols-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         <div>
-          <section className="px-[5%] py-6 md:py-8 lg:py-10">
-            <div className="container max-w-lg">
+          <section className="px-[5%] py-1">
+            <div className="container max-w-lg border-4 border-gray-100 rounded-lg p-4 bg-muted/60">
               <div className="mx-auto mb-8 w-full max-w-lg text-center md:mb-10 lg:mb-12">
                 {/* <p className="mb-3 font-semibold md:mb-4">Connect</p> */}
-                <h2 className="rb-5 mb-5 text-3xl font-bold md:mb-6 md:text-5xl lg:text-6xl">
+                <h2 className="rb-5 mb-5 text-3xl text-blue-800 font-bold md:mb-6 md:text-5xl lg:text-6xl">
                   Get in Touch
                 </h2>
                 <p className="md:text-md">We'd love to hear from you!</p>
@@ -53,20 +53,22 @@ const ContactUs = () => {
                   </Label>
                 </div>
                 <div className="text-center">
-                  <Button title="Submit">Submit</Button>
+                <Button className="px-6 font-semibold text-white text-sm md:text-base bg-redFlag hover:bg-pink-800 rounded-md transition-colors">
+                  Submit
+                </Button>
                 </div>
               </form>
             </div>
           </section>
         </div>
 
-        <div className="col-span-2 w-full p-2">
-          <section className="w-full mx-auto p-5 overflow-hidden items-center justify-center bg-gray-100">
+        <div className="col-span-2 w-full py-1">
+          <section className="w-full mx-auto p-5 overflow-hidden items-center justify-center bg-muted/60 border-4 border-gray-100 rounded-lg">
             <h1 className="text-2xl md:text-4xl lg:text-5xl font-extrabold text-blue-800 text-center">
               Contact Us
             </h1>
-            <hr className="w-48 h-1 mx-auto my-4 bg-blue-400 border-0 rounded md:my-10 dark:bg-gray-700" />
-            <div className="relative grid grid-cols-4 justify-center items-center border-4 border-gray-100 rounded-lg px-auto">
+            <hr className="w-48 h-1 mx-auto my-4 bg-blue-400 rounded md:my-10 dark:bg-gray-700" />
+            <div className="relative grid grid-cols-4 justify-center items-center rounded-lg px-auto">
               <div className="flex w-full items-start justify-center bg-sky-900">
                 <Image
                   src={NeuLogo}
@@ -103,10 +105,10 @@ const ContactUs = () => {
           </section>
 
           {/*Subs section */}
-          <section className="relative overflow-hidden border-4 border-gray-100 rounded-lg p-4 mt-5 md:p-5">
+          <section className="relative overflow-hidden border-4 border-gray-100 rounded-lg px-8 py-8 mt-8 bg-muted/60">
             <div className="flex flex-col md:flex-row items-start md:items-center gap-6 max-w-6xl mx-auto">
               <div className="flex-1 space-y-4">
-                <h2 className="text-2xl md:text-4xl font-bold">
+                <h2 className="text-2xl md:text-4xl text-blue-800 font-bold">
                   Don&apos;t Miss Our Updates
                 </h2>
                 <p className="text-sm md:text-base lg:text-lg leading-relaxed">
