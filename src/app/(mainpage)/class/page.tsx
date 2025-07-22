49e9d030c6bb3fa74894b5a106c603ec15f32819
ysamatha024 <ysamatha024@gmail.com>
2025-07-22 09:57:19 +0000

    Add Class Intro Page

---
+++ src/app/(mainpage)/class/page.tsx
@@ -0,0 +1,9 @@
+import ClassIntro from "./classIntro"
+
+export default function Class() {
+    return (
+      <main className="flex flex-col justify-center gap-10 py-20 overflow-hidden">
+        <ClassIntro />
+      </main>
+    );
+  }
